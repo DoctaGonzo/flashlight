6ac9e5d13ab5b82eea9e88b4661ad9b151e6a882
Tomi Piri <tomi_piri_484@hotmail.com>
2014-06-23 16:53:55 +0300

    Changed the layout.

--- Flashlight/src/fi/tomi/flashlight/MainActivity.java
+++ Flashlight/src/fi/tomi/flashlight/MainActivity.java
@@ -1,11 +1,13 @@
 package fi.tomi.flashlight;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.AlertDialog;
 import android.app.Fragment;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
@@ -16,22 +18,43 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ToggleButton;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.Switch;
+import android.widget.Toast;
 import android.os.Build;
 
 public class MainActivity extends Activity {
 
 	private final Context context = this;
 	private Camera camera;
-	private ToggleButton flashtoggle;
+	private Switch flashtoggle;
 	private PackageManager pm;
 
+	@SuppressLint("InlinedApi")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		View decorView = getWindow().getDecorView();
+		// Hide the status bar.
+		if (Build.VERSION.SDK_INT < 16) {
+			getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+					WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		} else {
+			int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+			decorView.setSystemUiVisibility(uiOptions);
+		}
+		// Remember that you should never show the action bar if the
+		// status bar is hidden, so hide that too if necessary.
+		ActionBar actionBar = getActionBar();
+		actionBar.hide();
 		setContentView(R.layout.activity_main);
 
-		flashtoggle = (ToggleButton) findViewById(R.id.flashtoggle);
+		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE);
+
+		flashtoggle = (Switch) findViewById(R.id.flashtoggle);
 		pm = context.getPackageManager();
 		if (!pm.hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
 			AlertDialog alertDialog = new AlertDialog.Builder(context).create();
@@ -48,6 +71,42 @@ public class MainActivity extends Activity {
 		} else {
 			camera = Camera.open();
 		}
+
+		if (!pm.hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)) {
+			flashtoggle.setChecked(false);
+			AlertDialog alertDialog = new AlertDialog.Builder(context).create();
+			alertDialog.setTitle("No Flash");
+			alertDialog.setMessage("Your device does not support flash.");
+			alertDialog.setButton(RESULT_OK, "Ok",
+					new DialogInterface.OnClickListener() {
+						public void onClick(final DialogInterface dialog,
+								final int which) {
+							Log.e("err", "Your device does not support flash.");
+						}
+					});
+			alertDialog.show();
+		} else {
+			final Parameters parameters = camera.getParameters();
+			flashtoggle
+					.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+						@Override
+						public void onCheckedChanged(CompoundButton buttonView,
+								boolean ischecked) {
+							if (ischecked) {
+								parameters
+										.setFlashMode(Parameters.FLASH_MODE_TORCH);
+								camera.setParameters(parameters);
+								camera.startPreview();
+							} else {
+								parameters
+										.setFlashMode(Parameters.FLASH_MODE_OFF);
+								camera.setParameters(parameters);
+								camera.stopPreview();
+							}
+						}
+					});
+		}
+
 	}
 
 	public void onFlashToggle(View view) {
@@ -66,7 +125,7 @@ public class MainActivity extends Activity {
 			alertDialog.show();
 		} else {
 			final Parameters parameters = camera.getParameters();
-			boolean isFlashOn = ((ToggleButton) view).isChecked();
+			boolean isFlashOn = ((Switch) view).isChecked();
 			if (!isFlashOn) {
 				parameters.setFlashMode(Parameters.FLASH_MODE_OFF);
 				camera.setParameters(parameters);
